ea624ea794091336b365a38efee71334147b6dbd
3oop <pooriaasareha@gmail.com>
2022-07-17 00:00:38 +0430

    bahojb naghes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # an Open Source CLI Massenger
 import os
-from typing import final
-salt = os.urandom(32)
 
+salt = os.urandom(32)
 from hashlib import pbkdf2_hmac
 from getpass import getpass
 
@@ -23,7 +22,7 @@ def register(name):
                 password = getpass("Enter a password: ")
                 password = salt + pbkdf2_hmac('sha256', password.encode('utf_8'), salt, 100000)
     with open("accounts.txt", "a") as accfile:
-        accfile.write(f"{name},{password}")
+        accfile.write(f"{name.ljust(32)},{password}")
         print(f"{name} registered.")
         return True
 
@@ -42,21 +41,54 @@ def login(name):
                 else:
                     print("Invalid Password! Get out.\n"+"+"*30)
                     return False
+                user = name
                 return True
             else:
                 print("Username not found")
                 return False
 
 
+def del_history():
+    if user == None:
+        print("No User is Logged in")
+        return False
+    with open(f"{user}.text", "w") as accfile:
+        accfile.flush()
+
+def acc_list():
+    print()
+
 def logout():
-    pass
+    user = None
 
+def exit():
+    run == False
 
+menu = {
+    '0': [exit , "Exit"],
+    '1': [login , "Login"],
+    '2': [register, "Register"],
+    '3': [logout, "Log out"],
+    '4': [del_history, "Delete History"],
+    '5': [acc_list, "Account List"]
+}
+
+user_menu = []
+user = None
 run = True 
 
 def main():
-    login(input("Username: "))
-    return True
+    while run == True:
+        if user == None:
+            print(*[f"{i} : {menu[i][1]}" for i in ['0', '1', '2'] ], sep='\n')
+        else:
+            print(*[f"{i} : {menu[i][1]}" for i in ['3', '4'] ], sep='\n')
+            with open(f"{user}.txt", 'r') as accfile:
+                eval(f"user_name = {accfile.readline()}")
+            print("Chats:")
+            print(*[f"{i}" for i in user_menu])
+            
+
 
 if __name__ == "__main__":
     main()
